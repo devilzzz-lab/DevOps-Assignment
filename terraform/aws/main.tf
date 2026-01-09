7eaac7344850c44cbe3621c64b9e4bb671e94391
devilzzz-lab <srisuji0814@gmail.com>
2026-01-10 00:41:06 +0530

    deploy: main deploy v1

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+  backend "s3" {
+    bucket         = "devops-assignment-784154679353-us-east-1-tf-state"
+    key            = "aws/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
+
 
 provider "aws" {
   region = var.aws_region
