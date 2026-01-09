a2e5f33b2f76bbd8d0627f4aceaafb4272a57602
devilzzz-lab <srisuji0814@gmail.com>
2026-01-09 21:39:45 +0530

    testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,5 +23,5 @@ async def health_check():
 
 @app.get("/api/message")
 async def get_message():
-    return {"message": "CD test successful – deployed from main branch"}
+    return {"message": "CD Test successful – deployed from main branch"}
  
