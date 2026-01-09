4dca2b908ddd34d648c8c74df3d6e2643bf3a968
devilzzz-lab <srisuji0814@gmail.com>
2026-01-09 14:05:46 +0530

    frontend finished

---
+++ frontend/jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Point to project root (NOT tests folder)
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+}
+
+module.exports = createJestConfig(customJestConfig)
